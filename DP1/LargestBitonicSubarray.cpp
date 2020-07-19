558e00cfda5c8ffd5eb5368e73f959165313b5b0
unknown <dilshadobryan@gmail.com>
2020-07-19 19:21:31 +0530

    Largest Bitonic Subarray

---
+++ DP1/LargestBitonicSubarray.cpp
@@ -0,0 +1,89 @@
+/*
+You are given an array of positive integers as input. Write a code to return the length of the largest such subsequence in which the values are arranged first in strictly ascending order and then in strictly descending order.
+Such a subsequence is known as bitonic subsequence. A purely increasing or purely decreasing subsequence will also be considered as a bitonic sequence with the other part empty.
+Note that the elements in bitonic subsequence need not be consecutive in the given array but the order should remain same.
+Input Format:
+Line 1 : A positive Integer N, i.e., the size of array
+Line 2 : N space-separated integers as elements of the array 
+Output Format:
+Length of Largest Bitonic subsequence
+Input Constraints:
+1<= N <= 100000
+Sample Input 1:
+6
+15 20 20 6 4 2
+Sample Output 1:
+5
+Sample Output 1 Explanation:
+Here, longest Bitonic subsequence is {15, 20, 6, 4, 2} which has length = 5.
+Sample Input 2:
+2
+1 5
+Sample Output 2:
+2
+Sample Input 3:
+2
+5 1
+Sample Output 3:
+2
+*/
+
+//code
+ 	
+	int longestBitonicSubarray(int *input, int n) {
+		
+	/* Don't write main().
+     	* the input is already passed as function argument.
+     	* Taking input and printing output is handled automatically.
+     	*/
+    
+        int* incoutp = new int[n];
+        int* decoutp = new int[n];
+        incoutp[0]=1;
+        decoutp[n-1]=1;
+        for(int i=1;i<n;i++){
+            incoutp[i]=1;
+            for(int j=i-1;j>=0;j--){
+                
+                if(input[j]>=input[i]){
+                    continue;
+                }
+                int possible = incoutp[j]+1;
+                if(possible>incoutp[i]){
+                    incoutp[i]=possible;
+                }
+            }
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            decoutp[i]=1;
+            for(int j=i+1;j<=n-1;j++){
+                
+                if(input[j]>=input[i]){
+                    continue;
+                }
+                int possible = decoutp[j]+1;
+                if(possible>decoutp[i]){
+                    decoutp[i]=possible;
+                }
+            }
+        }
+        
+        // for(int i=0;i<n;i++){
+        //     cout<<decoutp[i]<<" ";
+        // }
+        
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int x= incoutp[i]+decoutp[i]-1;
+            if(x>ans){
+                ans =x;
+            }
+        }
+     	
+     	
+        
+        return ans;
+     	
+ 	}
+
